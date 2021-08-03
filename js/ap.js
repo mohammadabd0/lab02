6244249d6fd6bef2988308c37830d687ca2ff318
mohammadabd0 <mohammadabdo518@gmail.com>
2021-08-03 16:10:53 +0300

    lab03

--- js/ap.js
+++ js/ap.js
@@ -1,8 +1,9 @@
 "use strict";
+let counter =0
 
-let nameof = prompt("what is your name")
+ let nameof = prompt("what is your name")
 
-alert("welcome to my web site: "+" "+nameof)
+ alert("welcome to my web site: "+" "+nameof)
 
 let name = prompt("is my name mohammad abdo ")
 if (name !== null) {
@@ -13,6 +14,7 @@ case 'yes':
 case 'y':
     alert("good job its right")
     console.log('your answer right');
+    counter++
     break;
     case 'no':
     case 'n':
@@ -30,6 +32,7 @@ switch(fact){
       alert("good job its right")
       console.log('your answer right');
       break;
+      counter++
       case 'no':
     case 'n':
         alert("your answer is wrong") 
@@ -42,6 +45,7 @@ if (ltuc !== null) {
 switch(ltuc){
     case 'yes':
     case 'y':
+        counter++
       alert("good job its right")
       console.log('your answer right');
       break;
@@ -63,6 +67,7 @@ switch(study){
     case 'n':
         alert("good job its right")
         console.log('your answer right');
+        counter++
         break;
 }
 let com = prompt("is my dream to have my own company")
@@ -74,9 +79,49 @@ switch(com){
     case 'y':
       alert("good job its right")
       console.log('your answer right');
+      counter++
       break;
       case 'no':
     case 'n':
         alert("your answer is wrong") 
         break;
 }
+
+
+let q6 =prompt("Number guessing game");
+
+for(let i=0;i<4;i++){
+    
+    if(q6>5){
+        console.log('too high');
+        alert('too high');
+    }else if(q6<5){
+        alert('too low');
+    }else if(q6==5){
+        alert("greate job")
+        counter++
+    }
+    q6 =prompt("Number guessing game")
+}
+let guessed = false;
+let car = ['camry', 'hyundai', 'Kia'];
+let answer = prompt('enter a car type');
+for(let j=0;j<6;j++){
+    for (let i = 0; i < car.length; i++) {
+        if (answer === car[i]) {
+            alert("yes its true");
+            guessed = true;
+            counter++
+            
+        }
+
+    }
+if(guessed==true){
+    break;
+}else{
+alert("try again")
+answer = prompt('enter a car type');
+}
+
+}
+alert("your final score is: "+ counter)
