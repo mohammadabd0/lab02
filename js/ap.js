7dda74916d8812a383ec2039d0987523b45871d3
mohammadabd0 <mohammadabdo518@gmail.com>
2021-08-07 22:41:37 +0300

    update

--- js/ap.js
+++ js/ap.js
@@ -1,148 +1,148 @@
-"use strict";
-let counter =0
-function f1(){
- let nameof = prompt("what is your name")
+// "use strict";
+// let counter =0
+// function f1(){
+//  let nameof = prompt("what is your name")
 
- alert("welcome to my web site: "+" "+nameof)
+//  alert("welcome to my web site: "+" "+nameof)
 
-let name = prompt("is my name mohammad abdo ")
-if (name !== null) {
-    name = name.toLowerCase();
-}
-switch (name){
-case 'yes':
-case 'y':
-    alert("good job its right")
-    console.log('your answer right');
-    counter++
-    break;
-    case 'no':
-    case 'n':
-        alert("your answer is wrong")
-        break;
+// let name = prompt("is my name mohammad abdo ")
+// if (name !== null) {
+//     name = name.toLowerCase();
+// }
+// switch (name){
+// case 'yes':
+// case 'y':
+//     alert("good job its right")
+//     console.log('your answer right');
+//     counter++
+//     break;
+//     case 'no':
+//     case 'n':
+//         alert("your answer is wrong")
+//         break;
         
-}
-}
-f1();
-function f2(){
+// }
+// }
+// f1();
+// function f2(){
     
 
-let fact = prompt("do i hate fish")
-if (fact !== null) {
-    fact = fact.toLowerCase();
-}
-switch(fact){
-    case 'yes':
-    case 'y':
-      alert("good job its right")
-      console.log('your answer right');
-      break;
-      counter++
-      case 'no':
-    case 'n':
-        alert("your answer is wrong") 
-        break;
-}
-}f2();
-function f3(){
+// let fact = prompt("do i hate fish")
+// if (fact !== null) {
+//     fact = fact.toLowerCase();
+// }
+// switch(fact){
+//     case 'yes':
+//     case 'y':
+//       alert("good job its right")
+//       console.log('your answer right');
+//       break;
+//       counter++
+//       case 'no':
+//     case 'n':
+//         alert("your answer is wrong") 
+//         break;
+// }
+// }f2();
+// function f3(){
     
 
-let ltuc = prompt("do i love ltuc")
-if (ltuc !== null) {
-    ltuc = ltuc.toLowerCase();
-}
-switch(ltuc){
-    case 'yes':
-    case 'y':
-        counter++
-      alert("good job its right")
-      console.log('your answer right');
-      break;
-      case 'no':
-    case 'n':
-        alert("your answer is wrong") 
-        break;
-}
-}f3();
-function f4(){
-let study = prompt("do i study hard ")
-if (study !== null) {
-    study = study.toLowerCase();
-}
-switch(study){
-    case 'yes':
-    case 'y':
-        alert("your answer is wrong") 
-      break;
-      case 'no':
-    case 'n':
-        alert("good job its right")
-        console.log('your answer right');
-        counter++
-        break;
-} 
-} f4();
-function f5(){
-let com = prompt("is my dream to have my own company")
-if (com !== null) {
-    com = com.toLowerCase();
-}
-switch(com){
-    case 'yes':
-    case 'y':
-      alert("good job its right")
-      console.log('your answer right');
-      counter++
-      break;
-      case 'no':
-    case 'n':
-        alert("your answer is wrong") 
-        break;
-}
-} f5();
-function f6(){
+// let ltuc = prompt("do i love ltuc")
+// if (ltuc !== null) {
+//     ltuc = ltuc.toLowerCase();
+// }
+// switch(ltuc){
+//     case 'yes':
+//     case 'y':
+//         counter++
+//       alert("good job its right")
+//       console.log('your answer right');
+//       break;
+//       case 'no':
+//     case 'n':
+//         alert("your answer is wrong") 
+//         break;
+// }
+// }f3();
+// function f4(){
+// let study = prompt("do i study hard ")
+// if (study !== null) {
+//     study = study.toLowerCase();
+// }
+// switch(study){
+//     case 'yes':
+//     case 'y':
+//         alert("your answer is wrong") 
+//       break;
+//       case 'no':
+//     case 'n':
+//         alert("good job its right")
+//         console.log('your answer right');
+//         counter++
+//         break;
+// } 
+// } f4();
+// function f5(){
+// let com = prompt("is my dream to have my own company")
+// if (com !== null) {
+//     com = com.toLowerCase();
+// }
+// switch(com){
+//     case 'yes':
+//     case 'y':
+//       alert("good job its right")
+//       console.log('your answer right');
+//       counter++
+//       break;
+//       case 'no':
+//     case 'n':
+//         alert("your answer is wrong") 
+//         break;
+// }
+// } f5();
+// function f6(){
 
-let q6 =prompt("Number guessing game");
+// let q6 =prompt("Number guessing game");
 
-for(let i=0;i<4;i++){
+// for(let i=0;i<4;i++){
     
-    if(q6>5){
-        console.log('too high');
-        alert('too high');
-    }else if(q6<5){
-        alert('too low');
-    }else if(q6==5){
-        alert("greate job")
-        counter++
-    }
-    q6 =prompt("Number guessing game")
-}
-alert("the correct answer is 5")
-} f6();
-function f7(){
-let guessed = false;
-let car = ['camry', 'hyundai', 'Kia'];
-let answer = prompt('enter a car type');
-for(let j=0;j<6;j++){
-    for (let i = 0; i < car.length; i++) {
-        if (answer === car[i]) {
-            alert("yes its true");
-            guessed = true;
-            counter++
+//     if(q6>5){
+//         console.log('too high');
+//         alert('too high');
+//     }else if(q6<5){
+//         alert('too low');
+//     }else if(q6==5){
+//         alert("greate job")
+//         counter++
+//     }
+//     q6 =prompt("Number guessing game")
+// }
+// alert("the correct answer is 5")
+// } f6();
+// function f7(){
+// let guessed = false;
+// let car = ['camry', 'hyundai', 'Kia'];
+// let answer = prompt('enter a car type');
+// for(let j=0;j<6;j++){
+//     for (let i = 0; i < car.length; i++) {
+//         if (answer === car[i]) {
+//             alert("yes its true");
+//             guessed = true;
+//             counter++
             
-        }
+//         }
 
-    }
-if(guessed==true){
-    break;
-}else{
-alert("try again")
-answer = prompt('enter a car type');
-}
+//     }
+// if(guessed==true){
+//     break;
+// }else{
+// alert("try again")
+// answer = prompt('enter a car type');
+// }
 
-}
-alert(" the correct answer is:  "+"  "+car)
-alert("your final score is: "+ counter)
-} f7();
+// }
+// alert(" the correct answer is:  "+"  "+car)
+// alert("your final score is: "+ counter)
+// } f7();
 
 
